bd2a895ee6e3cf696a750114afa18e05c3c27891
Raresh Nistor <me@thegreatrazz.nz>
2020-10-28 18:05:02 +1300

    Move back to .NET 4 and add a shim for MS Store

---
+++ VGAPainterWSShim/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VGAPainterWSShim
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() => VGAPainter.Program.Main();
+    }
+}
---
+++ VGAPainterWSShim/README.txt
@@ -0,0 +1,8 @@
+﻿
+VGAPainterWSShim
+----------------
+
+This project is a shim for VGAPainterWS to target a project using 
+.NET Framework >=4.6.1 while the original project staying at .NET Framework 4.
+
+The code is in ../VGAPainter and Microsoft Store bridge in ../VGAPainterWS.
